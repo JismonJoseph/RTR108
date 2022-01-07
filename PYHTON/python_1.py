6971f14b573c17b8d01f49fa92b55b69e5c402c0
JismonJoseph <94806946+JismonJoseph@users.noreply.github.com>
2022-01-07 19:20:18 +0200

    Delete PYHTON directory

--- PYHTON/python_1.py
+++
@@ -1,5 +0,0 @@
-no_hours = input("Enter the number of hours worked : ")
-rate = input("Enter the hourly pay : ")
-
-amount = float(no_hours) * float(rate)
-
--- PYHTON/python_2.py
+++
@@ -1,8 +0,0 @@
-no_hours = input("Enter the number of hours worked : ")
-rate = input("Enter the hourly pay : ")
-
-if float(no_hours) > 40:
-    t = float(no_hours) - 40
-    amount = (40 * float(rate)) + (t * 1.5 * float(rate))
-else:
-    amount = float(no_hours) * float(rate)
